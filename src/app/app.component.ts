d7cb7a7e0d6f26af2f9a256e5b4ea0dbf54010ea
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2023-02-06 22:14:52 +0100

    images added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'junusergin.de';
+export class AppComponent implements OnInit {
+ images = ['img-01.jpg', 'img-02.jpg', 'img-03.jpg', 'img-04.jpg', 'img-05.jpg'];
+currentImage = 0;
+showImage = true;
+
+ngOnInit() {
+this.updateImage();
 }
+
+updateImage() {
+ setInterval(() => {
+  this.currentImage++;
+  this.currentImage = this.currentImage % this.images.length;
+  this.showImage = false;
+
+  setTimeout(() => {
+    this.showImage = true;
+  }, 10);
+}, 8000);
+}
+
+}
+ 
